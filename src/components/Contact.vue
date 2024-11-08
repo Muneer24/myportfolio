aa4b1c2a4545d461bf88eaeafae13daf47f06574
Muneer <muneer.amari2@gmail.com>
2024-11-08 16:09:29 +0200

    testing

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -118,7 +118,7 @@
 
 
           <div class="md:hidden">
-              <div class="flex items-center">
+              <!--<div class="flex items-center">
                   <svg class="w-6 h-6 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                       <path
                           d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -144,14 +144,14 @@
                           d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
                       />
                   </svg>
-              </div>
+              </div>-->
 
               <blockquote class="mt-6">
                   <!--<p class="text-lg leading-relaxed text-white">You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change and click save.</p>-->
               </blockquote>
 
               <div class="flex items-center mt-8">
-                  <img class="flex-shrink-0 object-cover w-10 h-10 rounded-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/contact/4/avatar.jpg" alt="" />
+                  <img class="flex-shrink-0 object-cover w-10 h-10 rounded-full" src="../assets/Muneer.png" alt="" />
                   <div class="ml-4">
                       <p class="text-base font-semibold text-white">Muneer Doman</p>
                       <p class="mt-px text-sm text-gray-400">Full-Stack Dev</p>
